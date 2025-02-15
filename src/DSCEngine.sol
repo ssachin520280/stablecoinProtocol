2e0534abe714da71623a8078890df4f1a3036df5
Sachin Anand <ssachin520280@outlook.com>
2025-02-15 08:17:30 +0530

    depositCollateral WIP

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
 /**
  * @title DSCEngine (Decentralized Stablecoin Engine)
  * @author Sachin Anand
@@ -27,10 +30,64 @@ pragma solidity ^0.8.19;
  *
  * @custom:security-contact sachin@example.com
  */
-contract DSCEngine {
+contract DSCEngine is ReentrancyGuard {
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeOfSameLength();
+    error DSCEngine__NotAllowedToken();
+
+    mapping(address token => address priceFeed) private s_priceFeeds;
+    DecentralizedStableCoin private immutable i_dsc;
+
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DSCEngine__NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
+        _;
+    }
+
+    constructor(
+        address[] memory _tokenAddresses,
+        address[] memory _priceFeedAddresses,
+        address _dscAddress
+    ) {
+        if (_tokenAddresses.length != _priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeOfSameLength();
+        }
+        for (uint256 i = 0; i < _tokenAddresses.length; i++) {
+            s_priceFeeds[_tokenAddresses[i]] = _priceFeedAddresses[i];
+        }
+        i_dsc = DecentralizedStableCoin(_dscAddress);
+    }
+
     function depositCollateralAndMintDSC() external {}
 
-    function depositCollateral() external {}
+    /**
+    @notice Deposits collateral into the protocol to back DSC minting
+    @dev Follows CEI (Checks-Effects-Interactions) pattern
+    @param tokenCollateralAddress The ERC20 token address to be used as collateral
+    @param amountCollateral The amount of collateral to deposit (in wei)
+    @custom:requirements
+    - Token must be approved as collateral in constructor
+    - Amount must be > 0
+    - User must have approved this contract to spend their tokens
+     */
+    function depositCollateral(
+        address tokenCollateralAddress, 
+        uint256 amountCollateral
+    ) 
+        external 
+        moreThanZero(amountCollateral) 
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant {
+
+    }
 
     function redeemCollateralForDSC() external {}
 
