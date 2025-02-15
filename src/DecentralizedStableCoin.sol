c2fbe0b9537764917a67bbbc0480aee3e4e004b2
Sachin Anand <ssachin520280@outlook.com>
2025-02-15 07:27:07 +0530

    decentralizedStableCoin Smart contract

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -27,6 +27,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__MustBeMoreThanZero();
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
 
     constructor() ERC20("DecentalizedStableCoin", "DSC") Ownable(msg.sender) {}
 
@@ -39,6 +40,17 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
             revert DecentralizedStableCoin__BurnAmountExceedsBalance();
         }
         super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns(bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
     }
 
 }
